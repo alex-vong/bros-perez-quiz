a1bdec2607228e828e1993259a3bbd2057c99c29
Alex Vong <avong003@gmail.com>
2022-10-21 20:26:36 -0700

    added head and meta content

---
+++ head.php
@@ -0,0 +1,21 @@
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Perez Bros Quiz</title>
+		<meta
+			name="og:description"
+			content="See How Good of an Uncle or Aunt You Are!"
+		/>
+	
+		<meta property="og:title" content="The Ultimate Nephew Quiz" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content="https://alexvong.dev/images/bros-meta.png"  />
+		<meta property="og:image:type" content="https://alexvong.dev/images/bros-meta.png" />
+		<link rel="shortcut icon" type="image/png" href=[[]] />
+
+		<link rel='stylesheet' href='css/style.css'>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+

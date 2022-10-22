625144a0505d26584009463fe499274e82e06d95
Alex Vong <avong003@gmail.com>
2022-10-21 20:26:10 -0700

    added quiz questions

---
+++ quiz-data.php
@@ -0,0 +1,89 @@
+
+
+<?php 
+
+	$quiz = [
+		[
+			'questionNumber' => 'q1',
+			'question' => "When is Aisen's bday?",
+			'A' => 'September 6th, 2016',
+			'B' => 'September 8th, 2016', 
+			'C' => 'September 4th, 2016', 
+			'D' => 'September 2th, 2018', 
+		],
+		[
+			'questionNumber' => 'q2',
+			'question' => "When is Ronan's bday?",
+			'A' => 'September 29th, 2018',
+			'B' => 'September 26th, 2018', 
+			'C' => 'September 24th, 2016', 
+			'D' => 'September 22th, 2018', 
+		],
+		[
+			'questionNumber' => 'q3',
+			'question' => "When is Logan's bday?",
+			'A' => 'October 26th, 2021',
+			'B' => 'October 28th, 2021', 
+			'C' => 'October 29th, 2021', 
+			'D' => 'October 30th, 2021', 
+		],
+		[
+			'questionNumber' => 'q4',
+			'question' => "What is Aisen's favorite good?",
+			'A' => 'Strawberry Pancakes',
+			'B' => 'Spam, Eggs, and Rice', 
+			'C' => 'Blueberry Waffles', 
+			'D' => 'McDonald Happy Meal', 
+		],
+		[
+			'questionNumber' => 'q5',
+			'question' => "What is the name is Ronan's turtle?",
+			'A' => 'Stacey',
+			'B' => 'Shaquille', 
+			'C' => 'Sherry', 
+			'D' => 'Shelly', 
+		],
+		[
+			'questionNumber' => 'q6',
+			'question' => "What School Does Aisen Attend?",
+			'A' => 'Keith Elementary',
+			'B' => 'Kirkwood North Elementary', 
+			'C' => 'Kennedy-Williams Elementary', 
+			'D' => 'Kinsley Elementary', 
+		],
+		[
+			'questionNumber' => 'q7',
+			'question' => "What was the theme for Aisen's 5th and Ronan's 3rd bday party?",
+			'A' => 'Paw Patrol',
+			'B' => 'Under Water', 
+			'C' => 'Teenage Mutant Ninja Turles', 
+			'D' => 'Spiderman', 
+		],
+		[
+			'questionNumber' => 'q8',
+			'question' => "What is Currently Aisen's Favorite Game?",
+			'A' => 'MineCraft',
+			'B' => 'Red Ball 4', 
+			'C' => 'Red Ball 3', 
+			'D' => 'Angry Birds', 
+		],
+		[
+			'questionNumber' => 'q9',
+			'question' => "Who is Ronan's Favorite Super Hero?",
+			'A' => 'Hulk',
+			'B' => 'Captain America', 
+			'C' => 'Spider-Man', 
+			'D' => 'Batman', 
+		],
+		[
+			'questionNumber' => 'q10',
+			'question' => "What will be the Theme of Logan's 1st Bday Part?",
+			'A' => 'X-Men',
+			'B' => 'Jungle', 
+			'C' => 'Dinosaurs', 
+			'D' => 'Space', 
+		]
+	];
+
+
+ ?>
